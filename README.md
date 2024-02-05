687cb6ce5df46283d2a4c33e051648590364a3b1
burtically <158839888+burtically@users.noreply.github.com>
2024-02-05 21:53:17 +0200

    antihoag initial version

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 [Minqlx](https://github.com/MinoMino/minqlx) plugins used on the Quake For Newbies Quake Live servers.
 
-* `autohoag.py` - (WIP, not tested) when a player reaches a max winstreak of 3, they get moved to spectators.
-* `disable_ca.py` - disable votemap to the clan arena game mode.
-* `twitch.py` - a light fork of [irc.py](https://github.com/MinoMino/minqlx-plugins/blob/master/irc.py), bridges twitch chat and quake live chat. Twitch chat receives all chat, while in game only spectators receive twitch chat messages.
+* `autohoag.py`, by rinrekku - when a player reaches a max winstreak of 3, they get moved to spectators.
+* `disable_ca.py`, by burtically - disable votemap to the clan arena game mode.
+* `twitch.py`, by tumer - a light fork of [irc.py](https://github.com/MinoMino/minqlx-plugins/blob/master/irc.py), bridges twitch chat and quake live chat. Twitch chat receives all chat, while in game only spectators receive twitch chat messages.
--- antihoag.py
+++ antihoag.py
@@ -1,33 +1,35 @@
+# antihoag.py, a plugin that puts a player to spectators once he had reached the maximum consecutive number of wins (3).
 import minqlx
 
 class antihoag(minqlx.Plugin):
-  def __init__(self):
-      self.add_hook("game_end", self.handle_game_end)
-      self.add_hook("player_spawn", self.handle_player_spawn)
+    def __init__(self):
+        self.add_hook("game_end", self.handle_game_end)
 
-      self.set_cvar_once("qlx_maxWinStreak", "3")
 
-      self.winner_id = 0
-      self.winner_streak = 0
+        self.set_cvar_once("zmq_stats_enabled", "1")
+        self.set_cvar("qlx_maxWinStreak", "3")
+        self.maxWinStreak = int(self.get_cvar("qlx_maxWinStreak"))
 
-  def handle_game_end(self, *args, **kwargs):
-      scores = {}
-      for player in self.players():
-        scores[player.steam_id] = player.score
-      winner = max(scores, key=lambda k: scores[k])
-      if winner:
-        self.winner_id = winner.steam_id
-        self.winner_streak += 1
+        self.winner_id = 0
+        self.winner_streak = 0
 
-  def handle_player_spawn(self, player):
-      players_on = [p for p in self.players() if p.team in "free"]
-      if len(players_on) >= 3:
-          limit = int(self.get_cvar("qlx_maxWinStreak"))
-          if limit > 0 and self.winner_streak >= limit and player.steam_id == self.winner_id:
-            player.put('spectator')
-            self.winner_streak = 0
-            self.msg("{} has reached the consecutive win limit of {} and has been moved to spectators."
-                     .format(player.name, limit))
-          print(f"here we're comparing values : limit is {limit}, current streak is {self.winner_streak}, and spawning player is {player.name}")
-      else:
-        print("DEBUG LINE : SPANW HAPPENS BUT PLUGINS DONT CARE SINCE PLAYERS < 3")
+
+    def handle_game_end(self, data):
+        if data["ABORTED"]: return
+        scores = {}
+        for player in self.players():
+            if player.team != 'spectator':
+                scores[player.steam_id] = player.score
+        winner = max(scores, key=lambda k: scores[k])
+        if winner == self.winner_id:
+            self.winner_streak +=1
+        else:
+            self.winner_streak = 1
+            self.winner_id = winner
+        if self.winner_streak >= self.maxWinStreak:
+            self.winner_streal = 0
+            self.winner_id = 0
+            for player in self.players():
+                if player.steam_id == winner:
+                    self.msg("{} has reached the winstreak limit of ^1{}^7 and has been put ^1back in line^7".format(player.name, self.maxWinStreak))
+                    player.put('spectator')
