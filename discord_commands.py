6292c01c73a7bc27f96f2c4511b6b22c3acbb1d0
tumulustucru <136928609+tumulustucru@users.noreply.github.com>
2025-03-03 17:40:07 +0100

    Update discord commands redirect link
    Updated link to quakenewbies.com redirector

--- discord_commands.py
+++ discord_commands.py
@@ -42,7 +42,7 @@ class discord_commands(minqlx.Plugin):
                         server_ip = self.server_ip + ":" + str(self.get_cvar("net_port"))
                         role_id = int(self.get_cvar("qlx_discord_role_id"))
                         target_elo = self.fetch(player)
-                        content = f"<@&{role_id}> | {self.strip_quake_colors(str(player))} (ELO : {target_elo['elo']}, {target_elo['games']} games) is looking for {game_mode} on [{sv_hostname}](https://connectsteam.me/?{server_ip}) !!!"
+                        content = f"<@&{role_id}> | {self.strip_quake_colors(str(player))} (ELO : {target_elo['elo']}, {target_elo['games']} games) is looking for {game_mode} on [{sv_hostname}](https://quakenewbies.com/connect/{server_ip}) !!!"
                         req = requests.post("https://discordapp.com/api/channels/" +  self.discord_lfg_channel_id + "/messages",
                                                     data=json.dumps({'content': content}),
                                                     headers={'Content-type': 'application/json', 'Authorization': 'Bot ' + self.discord_bot_token})
